12b1900fa18abdd8035f79439cc2b98c38628890
Tixo Lantool <>
2024-04-02 19:05:55 +0100

    Updated look and pages

--- script.js
+++
@@ -1,7 +0,0 @@
-function cropSVG(svg) {
-    var  bbox = svg.getBBox();
-    svg.setAttribute("width", bbox.x + bbox.width + bbox.x);
-    svg.setAttribute("height", bbox.y + bbox.height + bbox.y);
-}
-
-window.onload = () => { cropSVG(document.getElementById("tixo-logo"));  }
